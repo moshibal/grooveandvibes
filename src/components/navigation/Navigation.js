1d3c47a32a21e085a1dfbc4aff9c34664e0fd26b
Bishal karki <bishalcarki2020@gmail.com>
2023-06-03 12:37:41 +1000

    for few times

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Navigation.css";
 import { toggleHamburger } from "./index.js";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 
 const Navigation = () => {
-  const navigate = useNavigate();
-  const { userInfo } = useSelector((state) => state.login);
-  const [selectedOption, setSelectedOption] = useState("");
+  // const navigate = useNavigate();
+  // const { userInfo } = useSelector((state) => state.login);
+  // const [selectedOption, setSelectedOption] = useState("");
 
-  const handleSelectOption = (e) => {
-    setSelectedOption(e.target.value);
-  };
-  useEffect(() => {
-    if (selectedOption === "register") {
-      // Logic for navigating to the Register page
-      navigate("/registration");
-    } else if (selectedOption === "bookingList") {
-      // Logic for navigating to the Booking List page
-      navigate("/bookings");
-    } else if (selectedOption === "studentList") {
-      // Logic for navigating to the Student List page
-      navigate("/studentList");
-    }
-  }, [navigate, selectedOption]);
+  // const handleSelectOption = (e) => {
+  //   setSelectedOption(e.target.value);
+  // };
+  // useEffect(() => {
+  //   if (selectedOption === "register") {
+  //     // Logic for navigating to the Register page
+  //     navigate("/registration");
+  //   } else if (selectedOption === "bookingList") {
+  //     // Logic for navigating to the Booking List page
+  //     navigate("/bookings");
+  //   } else if (selectedOption === "studentList") {
+  //     // Logic for navigating to the Student List page
+  //     navigate("/studentList");
+  //   }
+  // }, [navigate, selectedOption]);
 
   return (
     <>
@@ -34,7 +34,7 @@ const Navigation = () => {
         <div className="right">
           <Link href="#booking">Booking</Link>
           <Link href="tel:0433756731">Contact</Link>
-          {userInfo?.data?.email === "manial@gmail.com" ? (
+          {/* {userInfo?.data?.email === "manial@gmail.com" ? (
             <select
               className="nav-dropdown"
               value={selectedOption}
@@ -47,7 +47,7 @@ const Navigation = () => {
             </select>
           ) : (
             <Link to="/login">Admin</Link>
-          )}
+          )} */}
           <Link
             href="https://www.tiktok.com/@grooveandvibes"
             target="_blank"
@@ -72,7 +72,7 @@ const Navigation = () => {
       <nav className="mobileNav">
         <Link href="tel:0433756731">Contact</Link>
         <Link href="#booking">Booking</Link>
-        <Link to="/login">Admin</Link>
+        {/* <Link to="/login">Admin</Link> */}
         <Link
           href="https://www.tiktok.com/@grooveandvibes"
           target="_blank"
